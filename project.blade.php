17088250347f52043283f2b4a13289ca5a2a6ad0
bily81 <72961580+bily81@users.noreply.github.com>
2021-08-13 23:45:27 +0700

    Add files via upload

---
+++ project.blade.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <!--navbar-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+          <div class="container">
+          <a class="navbar-brand navbar-link" href=""><em><strong>TUGAS</strong></em></a>
+          <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item " role="presentation">
+                <a class="nav-link ml-3" href="#BERANDA">BERANDA</span></a>
+              </li>
+              <li class="nav-item" role="presentation">
+                <a class="nav-link ml-3" href="#PROFILE">PROFILE</a>
+              </li>
+              <li class="nav-item" role="presentation">
+                <a class="nav-link ml-3" href="#GALERI">GALERI</a>
+              </li>
+            </ul>
+          </div>
+        </div> 
+        </nav>
+<!--navbar-->
+
+<!--BERANDA-->
+  <div id="BERANDA">
+      <div 
+        class="bg-image"
+        style="background-image: url('img/bg3.jpg');
+        height: 650px">
+        <div class="text-center ">
+        <div class="mask" style="height: 649px; background-color: rgba(0,0,0,0.6);">
+          <div class="d-flex justify-content-center align-items-center h-100">
+            <div class="text-white">
+              <h1 class="mb-3">Welcome</h1>
+              <h4 class="mb-3">To My WEB</h4>
+              <a class="btn btn-outline-light btn-lg"href="#PROFILE">Please See</a>
+              </div>
+           </div>
+          </div>
+        </div>
+      </div>
+  </div>
+<!--BERANDA-->
+
+
+<!--PROFILE-->
+  <div id="PROFILE">
+    <div class="bg-info">
+      <h1 class="text-center"><b>PROFILE</b></h1>
+        <div class="container">
+          <img src="img/img2.jpg" style="height: 300px" class="mt-5 mx-auto d-block img-thumbnail">
+          <h4 class="text-center"><em>AYU DIAN RAHMADHANI | 06 | XII RPL 1</em></h4>
+          <img src="img/img4.jpg" style="height: 300px" class="mt-5 mx-auto d-block img-thumbnail">
+          <h4 class="text-center"><em>MUHAMMAD SABILILLAH SYAHBANA | 24 | XII RPL 1</em></h4>
+        </div>
+    </div>
+  </div>
+<!--PROFILE-->
+
+
+<!--GALERI-->
+      <div id="GALERI">
+      <h1 class="text-center"><b>GALERI</b></h1>
+      <hr>
+
+      <div class="container">
+        <div class="row mt-5">
+          <div class="col-md-4">
+            <a href="">
+              <img src="img/img1.jpg" class="img-thumbnail">
+            </a>
+            </div>
+            <div class="col-md-4">
+              <a href="">
+                <img src="img/img1.jpg"  class="img-thumbnail">
+              </a>
+              </div>
+              <div class="col-md-4">
+                <a href="">
+                  <img src="img/img1.jpg" class="img-thumbnail">
+                </a>
+                </div>
+                      <div class="col-md-4">
+                        <a href="">
+                          <img src="img/img3.jpg" class="img-thumbnail">
+                        </a>
+                        </div>
+                        <div class="col-md-4">
+                          <a href="">
+                            <img src="img/img3.jpg"  class="img-thumbnail">
+                          </a>
+                          </div>
+                          <div class="col-md-4">
+                            <a href="">
+                              <img src="img/img3.jpg" class="img-thumbnail">
+                            </a>
+                            </div>
+          </div>
+        </div>
+      </div>
+<!--GALERI-->
+
+     </div>
+   </div>
+ </div>
+
+        
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
